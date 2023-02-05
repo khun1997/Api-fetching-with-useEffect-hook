64ab8a8ff2ddf6daabdee3e2b9228119a7bba79d
Khun Ye Aung <kkhun531@gmail.com>
2023-02-05 23:53:01 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
+// function App() {
+//     // 2. Create our *dogImage* variable as well as the *setDogImage* function via useState
+//     // We're setting the default value of dogImage to null, so that while we wait for the
+//     // fetch to complete, we dont attempt to render the image
+//   let [dogImage, setDogImage] = useState(null)
+
+//     // 3. Create out useEffect function
+//   useEffect(() => {
+//     fetch("https://dog.ceo/api/breeds/image/random")
+//     .then(response => response.json())
+//         // 4. Setting *dogImage* to the image url that we received from the response above
+//     .then(data => setDogImage(data.message))
+//   },[])
+
+//   return (
+//     <div className="App">
+//         {/* 5. Using *dogImage as* the *src* for our image*/}
+//     {dogImage && <img src={dogImage}></img>}
+//     </div>
+//   );
+// }
+
 function App() {
     // 2. Create our *dogImage* variable as well as the *setDogImage* function via useState
     // We're setting the default value of dogImage to null, so that while we wait for the
@@ -12,7 +34,7 @@ function App() {
 
     // 3. Create out useEffect function
   useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://dog.ceo/api/breeds/image/random/3")
     .then(response => response.json())
         // 4. Setting *dogImage* to the image url that we received from the response above
     .then(data => setDogImage(data.message))
@@ -20,8 +42,8 @@ function App() {
 
   return (
     <div className="App">
-        {/* 5. Using *dogImage as* the *src* for our image*/}
-    {dogImage && <img src={dogImage}></img>}
+        {/* 5. Returning an img element for each url, again with the value of our src set to the image url */}
+    {dogImage && dogImage.map((dog) => <img width={"200px"} margin={"10px"} height={"200px"} src={dog}></img>)}
     </div>
   );
 }
