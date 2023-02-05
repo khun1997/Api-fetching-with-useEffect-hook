c0e89683c202af94fe4428cec8c9db5c36bd2d61
Khun Ye Aung <kkhun531@gmail.com>
2023-02-05 23:40:40 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import './App.css';
+
+// 1. Import *useState* and *useEffect*
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+    // 2. Create our *dogImage* variable as well as the *setDogImage* function via useState
+    // We're setting the default value of dogImage to null, so that while we wait for the
+    // fetch to complete, we dont attempt to render the image
+  let [dogImage, setDogImage] = useState(null)
+
+    // 3. Create out useEffect function
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+    .then(data => setDogImage(data.message))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* 5. Using *dogImage as* the *src* for our image*/}
+    {dogImage && <img src={dogImage}></img>}
     </div>
   );
 }
+
 
 export default App;
